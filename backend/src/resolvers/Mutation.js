dae608acf2bd1349631952222226b0a2ef16c306
Victor Marmolejo <victor@targetcw.com>
2019-02-28 21:51:55 -0800

    Revert "test"
    This reverts commit 32c361b90faec048ba99f9037df295a08247eefa.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -116,7 +116,6 @@ const mutations = {
 		if (!valid) {
 			throw new Error('Contraseña Invalida!');
 		}
-		throw new Error(`Usuario ------> ${user.name} `);
 		// generate the JWT token
 		const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
 		// set the cookie with the token
